624b0c2732c02631811876e9ce5cdfe9588b9b51
Ricardo Chavez <ricardo.chavez.t@gmail.com>
2024-12-24 00:28:49 +0200

    Day 21 part 1 improved

--- day21.py
+++ day21.py
@@ -51,69 +51,38 @@ def generate_inputs(code):
         gap_coordinates = (0,0)
     
     prev_coordinates = button_coordinates("A")
-    input_list = [[]]
+    input_list = []
     for c in code:
         coordinates = button_coordinates(c)
         drow, dcol = coordinates[0]-prev_coordinates[0], coordinates[1]-prev_coordinates[1]
         horiz_input = (">" if dcol > 0 else "<") * abs(dcol)
         vert_input = ("v" if drow > 0 else "^") * abs(drow)
-        if dcol == 0 or drow == 0:
-            for l in input_list:
-                l.append(horiz_input)
-                l.append(vert_input)
-                l.append("A")
+        
+        if dcol < 0:
+            if (prev_coordinates[0], prev_coordinates[1] + dcol) == gap_coordinates:
+                input_list.append(vert_input + horiz_input + "A")
+            else:
+                input_list.append(horiz_input + vert_input + "A")
         else:
-            new_input_list = []
-            for l in input_list:
-                if (prev_coordinates[0], prev_coordinates[1]+dcol) != gap_coordinates:
-                    new_input_list.append(l + [horiz_input, vert_input, "A"])
-                if (prev_coordinates[0]+drow, prev_coordinates[1]) != gap_coordinates:
-                    new_input_list.append(l + [vert_input, horiz_input, "A"])
-            input_list = new_input_list
+            if (prev_coordinates[0] + drow, prev_coordinates[1]) == gap_coordinates:
+                input_list.append(horiz_input + vert_input + "A")
+            else:
+                input_list.append(vert_input + horiz_input + "A")
+
         prev_coordinates = coordinates
     
-    return ["".join(l) for l in input_list]
+    return "".join(input_list)
 
 if __name__ == "__main__":
     codes = [line.strip() for line in fileinput.input()]
 
     sum_complexities = 0
     for code in codes:
-        inputs = [code]
+        input = code
         for n in range(3):
-            new_inputs = []
-            min_len = 2 ** 32
-            for input in inputs:
-                for new_input in generate_inputs(input):
-                    if len(new_input) < min_len:
-                        new_inputs = [new_input]
-                        min_len = len(new_input)
-                    elif len(new_input) == min_len:
-                        new_inputs.append(new_input)
+            input = generate_inputs(input)
 
-            inputs = new_inputs
-
-        print("Complexity:", min_len, "*", int(code[:-1]))
-        sum_complexities += (min_len * int(code[:-1]))
-
-        # i1 = sorted(generate_inputs(code), key=len)
-        # i2 = []
-        # min_len = 2 ** 32
-        # for input in i1:
-        #     for new_input in generate_inputs(input):
-        #         if len(new_input) < min_len:
-        #             i2 = [new_input]
-        #             min_len = len(new_input)
-        #         elif len(new_input) == min_len:
-        #             i2.append(new_input)
-        # min_len = 2 ** 32
-        # for input in i2:
-        #     i3 = generate_inputs(input)
-        #     current_min_len = min([len(l) for l in i3])
-        #     if current_min_len < min_len:
-        #         min_len = current_min_len
-
-        # print("Complexity:", min_len, "*", int(code[:-1]))
-        # sum_complexities += (min_len * int(code[:-1]))
+        print("Complexity:", len(input), "*", int(code[:-1]))
+        sum_complexities += (len(input) * int(code[:-1]))
 
     print("Sum complexities:", sum_complexities)
